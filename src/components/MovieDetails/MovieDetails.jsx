8403a15f44633b98998e7b73f3d88797a0dc7adf
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2023-01-08 02:04:44 +0200

    /

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useParams, useLocation } from 'react';
+import { useState, useEffect, useLocation } from 'react';
+import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Loader } from 'components/Loader';
